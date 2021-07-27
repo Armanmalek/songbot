b2dc522c6334cb1c98a3cb8a9f29ac795d80442d
Armanmalek <amalekja@uoguelph.ca>
2021-07-27 18:06:21 -0400

    ad

--- lib/songbot_web/controllers/songlink_controller.ex
+++ lib/songbot_web/controllers/songlink_controller.ex
@@ -1,8 +1,12 @@
 defmodule SongbotWeb.SonglinkController do
   use SongbotWeb, :controller
+  import Plug.Conn
 
   def index(conn, params) do
-    IO.inspect(params)
+    IO.inspect(conn)
+    # whichBrand = Conn.get
     json(conn, %{"arman" => "erer"})
   end
+
+
 end
